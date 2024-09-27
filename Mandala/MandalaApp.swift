d227eaa20423646740c32817d0f05ff9a5cb2109
Benia Morgan <bmorganware@gmail.com>
2024-09-27 13:53:59 -0400

    Initial Commit

---
+++ Mandala/MandalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandalaApp.swift
+//  Mandala
+//
+//  Created by Benia Morgan-Ware on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MandalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
