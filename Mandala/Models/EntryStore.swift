ba486f45702df088d4c43534268324cd4e219cb3
Benia Morgan <bmorganware@gmail.com>
2024-10-14 18:54:18 -0400

    Added load, and save functions to the entry store. Users entries will save to disk when the scene phase of the backing is changed to the background

--- Mandala/Models/EntryStore.swift
+++ Mandala/Models/EntryStore.swift
@@ -19,10 +19,35 @@ class EntryStore:ObservableObject {
     }
     
     func load() {
-        
+        do {
+            let url = try archiveURL()
+            print("Loading entries from url: \(url)")
+            let data = try Data(contentsOf: url)
+            let unarchiver = PropertyListDecoder()
+            let entries = try unarchiver.decode([Entry].self, from: data)
+            allEntries = entries
+        } catch CocoaError.fileReadNoSuchFile {
+            //Ignore error
+        } catch let decodingError {
+            print("Could not read saved entries: \(decodingError)")
+        }
     }
     
     func save() {
+        do {
+            let url = try archiveURL()
+            print("Saving entries to url \(url)")
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(allEntries)
+            try data.write(to: url)
+            print("Saved entries all entries")
+        } catch let endcodingError {
+            print("Could not save entries: \(endcodingError)")
+        }
         
+    }
+    func archiveURL() throws -> URL {
+        let documentsDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        return documentsDirectory.appendingPathComponent("entries", conformingTo: .propertyList)
     }
 }
