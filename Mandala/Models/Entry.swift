2791dfbf2fe211c845325671bb305c5907a3c772
Benia Morgan <bmorganware@gmail.com>
2024-10-15 10:25:39 -0400

    Added ane xtentsion to the Entry model with an enum for coding key to setup manual encoding

--- Mandala/Models/Entry.swift
+++ Mandala/Models/Entry.swift
@@ -7,8 +7,24 @@
 
 import Foundation
 
-struct Entry: Identifiable, Codable {
+struct Entry: Identifiable {
     var mood: Mood
     var timestamp = Date()
     var id = UUID()
 }
+
+extension Entry: Codable {
+    enum CodingKeys: String, CodingKey {
+        case mood, timestamp, id
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(mood, forKey: .mood)
+        try container.encode(timestamp, forKey: .timestamp)
+        try container.encode(id, forKey: .id)
+    }
+}
+
+
