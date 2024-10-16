cddfcfb808f52e991a4e7894a076ec2e28e556b1
Benia Morgan <bmorganware@gmail.com>
2024-10-16 16:39:39 -0400

    Navigation destionation in entryList view has been implemented. Button to add new journal entry has been moved and added to toolbar

--- Mandala/Views/EntryListView.swift
+++ Mandala/Views/EntryListView.swift
@@ -11,6 +11,17 @@ struct EntryListView: View {
     @EnvironmentObject var store: EntryStore
     var body: some View {
         List {
+            ForEach(store.allEntries) { entry in
+                NavigationLink(value: entry) {
+                    EntryRowView(entry: entry)
+                }
+            }
+            .onDelete(perform: delete(at:))
+        }
+        .navigationDestination(for: Entry.self) { entry in
+            entry.mood.longName
+        }
+        .toolbar {
             Menu {
                 ForEach(Mood.allCases, id: \.self) { mood in
                     Button {
@@ -22,19 +33,10 @@ struct EntryListView: View {
             } label: {
                 Label("New Entry", systemImage: "plus")
             }
-            
-            ForEach(store.allEntries) { entry in
-                NavigationLink(value: entry) {
-                    EntryRowView(entry: entry)
-                }
-            }
-            .onDelete(perform: delete(at:))
-        }
-        .navigationDestination(for: Entry.self) { entry in
-            entry.mood.longName
         }
     
     }
+    
     func delete(at offsets: IndexSet) {
         store.allEntries.remove(atOffsets: offsets)
     }
@@ -49,6 +51,8 @@ struct EntryListView: View {
 
 #Preview {
     let store = EntryStore(entries: [Entry(mood: .getRandomMood()), Entry(mood: .getRandomMood()), Entry(mood: .getRandomMood())])
-    EntryListView()
-        .environmentObject(store)
+    NavigationStack {
+        EntryListView()
+            .environmentObject(store)
+    }
 }
